46f2e104acd3f871f379abe880e523766e9dfde8
AtomasFlow <atomas8888@gmail.com>
2023-04-06 16:00:26 +0300

    done 4 lessons

--- src/route/index.js
+++ src/route/index.js
@@ -5,6 +5,35 @@ const router = express.Router()
 
 // ================================================================
 
+var header = {
+  name: {
+    firstname: 'Ivan',
+    lastname: 'Ivanov',
+  },
+  position: 'Junior Fullstack JS Developer',
+  salary: '600$ в місяц',
+  address: 'Kirovogradskaya oblast city Svetlovodsk',
+}
+
+var footer = {
+  social: {
+    email: {
+      text: 'ivanov@mail.com',
+      href: 'malito:ivanov@mail.com',
+    },
+    phone: {
+      text: '+380670000123',
+      href: 'tel:+380670000123',
+    },
+    linkedin: {
+      text: 'LinkedIn',
+      href: 'https://www.linkedin.com/in/dmytro-test',
+    },
+  },
+}
+// ================================================================
+
+
 // router.get Створює нам один ентпоїнт
 
 //           ↙ тут вводимо шлях (PATH) до сторінки
@@ -23,10 +52,217 @@ router.get('/summary', function (req, res) {
   //             ↙ cюди вводимо назву файлу з сontainer
   res.render('summary', {
     // ↙ сюди вводимо JSON дані
+    page: {
+      title: 'Resume | Summary',
+    },
+
+    header,
+
+    main: {
+      summary: {
+        title: 'Summary',
+        text: `Open-minded for new technologies, with 1 years of experience in development.
+            Whenever I start to work on a new project I learn the domain and try to understand
+            the idea of the project. Good team player, every colleague is a friend to me.`,
+      },
+      experience: {
+        title: 'Other experience',
+        text: `Pet project for parsing sport betting data from different platforms ( odds )
+              and sport statistics (tournament position, goals etc), analyzing by simple
+              mathematics models and preparing probabilityfor such events
+              like: money line - first win / draw / second win, totals etc.`,
+      },
+    },
+
+    footer,
+  })
+})
+
+// ================================================================
+// ================================================================
+
+//              ↙ тут вводимо шлях (PATH) до сторінки
+router.get('/skills', function (req, res) {
+  //             ↙ cюди вводимо назву файлу з сontainer
+  res.render('skills', {
+    // ↙ сюди вводимо JSON дані
+    page: {
+      title: 'Resume | Skills',
+    },
+
+    header,
+
+    main: {
+      skills: [
+        {
+          name: 'HTML',
+          point: 10,
+          isTop: true,
+        },
+
+        {
+          name: 'Handlebars',
+          point: 10,
+          isTop: true,
+        },
+        {
+          name: 'VS Code',
+          point: 8,
+        },
+        {
+          name: 'Git',
+          point: 5,
+          isTop: false,
+        },
+        {
+          name: 'Terminal',
+          point: 8,
+        },
+        {
+          name: 'NPM',
+          point: 6,
+        },
+        {
+          name: 'React.js',
+          point: 0,
+        },
+        {
+          name: 'PHP',
+          point: null,
+        },
+      ],
+      hobbies: [
+        {
+          name: 'Sports',
+          isMain: true,
+        },
+        {
+          name: 'Run',
+          isMain: true,
+        },
+        {
+          name: 'Walk',
+          isMain: false,
+        },
+      ],
+    },
+
+    footer,
   })
 })
 
 // ================================================================
 
+//              ↙ тут вводимо шлях (PATH) до сторінки
+router.get('/education', function (req, res) {
+  //             ↙ cюди вводимо назву файлу з сontainer
+  res.render('education', {
+    // ↙ сюди вводимо JSON дані
+    page: {
+      title: 'Resume | Education',
+    },
+
+    header,
+
+    main: {
+      educations: [
+        {
+          name: 'Школа',
+          isEnd: true,
+        },
+        {
+          name: 'Училище №5',
+          isEnd: true,
+        },
+        {
+          name: 'Украинская инженерно-педагогическая академия',
+          isEnd: false,
+        },
+      ],
+      certificates: [
+        {
+          name: 'Level 1',
+          id: 45,
+        },
+        {
+          name: 'Level 2',
+          id: 388,
+        },
+        {
+          name: 'Level 3',
+          id: 1254,
+        },
+      ],
+    },
+
+    footer,
+  })
+})
+
+
+// ================================================================
+
+//              ↙ тут вводимо шлях (PATH) до сторінки
+router.get('/work', function (req, res) {
+  //             ↙ cюди вводимо назву файлу з сontainer
+  res.render('work', {
+    // ↙ сюди вводимо JSON дані
+    layout: 'big',
+
+    page: {
+      title: 'Resume | Work',
+    },
+
+    header,
+
+    main: {
+      works: [
+        {
+          position: 'Junior Fullstack Developer',
+          company: {
+            name: 'IT Brains',
+            url: 'https://it-brains.com.ua/',
+          },
+          duration: {
+            from: '10.10.2022',
+            to: null,
+          },
+          projectAmout: 3,
+          projects: [
+            {
+              name: 'Resume',
+              url: 'https://resume.com.ua/',
+              about:
+                'Here you can find activities to practise your reading skills.',
+              stacks: [
+                {
+                  name: 'React.js',
+                },
+                {
+                  name: 'HTML / CSS',
+                },
+                {
+                  name: 'Nodejs',
+                },
+              ],
+              awards: [
+                {
+                  name: 'Reading will help you to improve your understanding of the language and build your vocabulary.',
+                },
+                {
+                  name: `The self-study lessons in this section are written and organised by English level based on the Common European Framework of Reference for languages (CEFR).`,
+                },
+              ],
+              stackAmout: 8,
+              awardAmout: 3,
+            },
+          ],
+        },
+      ],
+    },
+
+    footer,
+  })
+})
 // Підключаємо роутер до бек-енду
 module.exports = router
