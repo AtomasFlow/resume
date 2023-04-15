7d9c4bd0cab1026c3b7b3a7abb9bbf1f4aac22bc
AtomasFlow <atomas8888@gmail.com>
2023-04-15 14:59:13 +0300

    some upd - add list

--- src/route/index.js
+++ src/route/index.js
@@ -1762,6 +1762,46 @@ router.get('/shophome', function (req, res) {
 
 // ================================================================
 
+// ================================================================
+
+
+// router.get Створює нам один ентпоїнт
+
+//           ↙ тут вводимо шлях (PATH) до сторінки
+router.get('/list', function (req, res) {
+  // res.render генерує нам HTML сторінку
+
+  //            ↙ cюди вводимо назву файлу з сontainer
+  res.render('list', {
+    layout: 'default',
+
+    list: [
+      { name: 'First Button' },
+      { name: 'Secondary Button' },
+      { name: 'Error Button' },
+      { name: 'Submit Button' },
+    ],
+
+    user: {
+      name: {
+        value: 'Ivan',
+        text: 'Ivanov',
+      },
+      role: {
+        text: 'Admin',
+      },
+      isConfirm: {
+        value: true,
+      },
+      age: {
+        value: 32,
+      },
+    },
+  })
+  //                  ↑↑ сюди вводимо JSON дані
+})
+
+// ================================================================
 
 // Підключаємо роутер до бек-енду
 module.exports = router
